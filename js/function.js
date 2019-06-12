a672e682ab2105b463c045ed767f16c6361aca1f
loverlhq <450597782@qq.com>
2019-06-12 10:50:02 +0800

    website V0.55
    add font family

--- js/function.js
+++ js/function.js
@@ -21,11 +21,25 @@
 //                      佛祖保佑       永无BUG
 //
 
+//当DOM元素加载完后执行
+window.onload = function () {
+    $(".animateFadeIn>img").fadeIn(500);
+
+
+    // 点击回到最顶端
+    $("#toTopBtn").click(function () {
+        // console.log("click");
+        $("body,html").animate({scrollTop:0},"linear");
+    })
+};
+
+
 // page init config
 $(".tabbar").load("tabbar.html");
 $(".footer").load("footer.html");
+$(".functionindex").load("function.html");
 
-
+// add box shadow
 $('ul .inforBox').hover(
     function () {
         $(this).addClass("boxShadow");
@@ -35,23 +49,29 @@ $('ul .inforBox').hover(
     }
 )
 
-window.onload = function() {
-    $(".animateFadeIn>img").fadeIn(500);
-};
-
 //select model
-$(".unline").on("click",function(e){
+$(".unline").on("click", function (e) {
     // console.log("runing");
     var sid = '';
-    if($(this).hasClass('RQ900')){
+    if ($(this).hasClass('RQ900')) {
         sid = 'RQ900';
         $('#RQ900').fadeIn().siblings().fadeOut();
-    }else if($(this).hasClass('RQ650')){
+    } else if ($(this).hasClass('RQ650')) {
         sid = 'RQ650';
         $('#RQ650').fadeIn().siblings().fadeOut();
-    }else if($(this).hasClass('mitsubishi')){
+    } else if ($(this).hasClass('mitsubishi')) {
         sid = 'mitsubishi';
         $('#mitsubishi').fadeIn().siblings().fadeOut();
     }
-    
+})
+
+
+//  判断滚动高度显示top按钮
+$(document).scroll(function () {
+    var scrollH = $(document).scrollTop();
+    if (scrollH > 100) {
+        $(".toTopBtn").fadeIn(100);
+    } else {
+        $(".toTopBtn").fadeOut(100);
+    }
 })
